cb6514f4b2ca51dae6355939c6e2268ba92909a2
Ariel Sardinha <ariel.sardinha@treinaweb.com.br>
2022-07-04 17:22:51 -0300

    aula2 video 3

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
 import { Button } from '@mui/material';
 import useOnlineStatus from './data/hooks/useOnlineStatus';
 
+import { ThemeProvider } from '@mui/material'
+import theme from './UI/theme/theme';
+
 function App() {
   const isOnline = useOnlineStatus()
 
 
   return (
     <div>
-      {isOnline ? 'Você está online' : 'Você está off-line'}
-      <Button variant='contained'>Clique aqui!</Button>
+      <ThemeProvider theme={theme}>
+        {isOnline ? 'Você está online' : 'Você está off-line'}
+        <Button variant='contained'>Clique aqui!</Button>
+
+      </ThemeProvider>
     </div>
   );
 }
