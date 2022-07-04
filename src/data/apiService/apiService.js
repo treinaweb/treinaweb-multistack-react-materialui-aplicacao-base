cce15e7128041d192e6a398447e32413e1a1eb27
Ariel Sardinha <ariel.sardinha@treinaweb.com.br>
2022-07-04 15:08:25 -0300

    aula1 video13

--- src/data/apiService/apiService.js
+++ src/data/apiService/apiService.js
@@ -1,9 +1,9 @@
-const baseUrl = 'http://localhost:3002/api'
+const baseUrl = 'http://localhost:3002/api/'
 
 
 export const ApiService = {
     get(endPoint = '') {
-        fetch(baseUrl + endPoint)
+        return fetch(baseUrl + endPoint)
             .then(response => response.json())
     }
 }
